80fd123af334d8d037cf76e911e1b40897cb891a
Kostoeva <irmareka@gmail.com>
2019-12-01 15:24:22 -0800

    adding main gun range scene

--- Assets/FPSHangar/Tutorial1/Scripts/SetSunLight.cs
+++ Assets/FPSHangar/Tutorial1/Scripts/SetSunLight.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class SetSunLight : MonoBehaviour {
 
-	public Renderer lightwall;
+	//public Renderer lightwall;
 
 	Material sky;
 
-	public Renderer water;
+	//public Renderer water;
 
 	public Transform stars;
 	public Transform worldProbe;
@@ -39,21 +39,21 @@ public class SetSunLight : MonoBehaviour {
 		if (lighton)
 		{
 			Color final = Color.white * Mathf.LinearToGammaSpace(5);
-			lightwall.material.SetColor("_EmissionColor", final);
-			DynamicGI.SetEmissive(lightwall, final);
+			//lightwall.material.SetColor("_EmissionColor", final);
+			//DynamicGI.SetEmissive(lightwall, final);
 		}
 		else
 		{
 			Color final = Color.white * Mathf.LinearToGammaSpace(0);
-			lightwall.material.SetColor("_EmissionColor", final);
-			DynamicGI.SetEmissive(lightwall, final);
+			//lightwall.material.SetColor("_EmissionColor", final);
+			//DynamicGI.SetEmissive(lightwall, final);
 		}
 	
 		Vector3 tvec = Camera.main.transform.position;
 		worldProbe.transform.position = tvec;
 
-		water.material.mainTextureOffset = new Vector2(Time.time / 100, 0);
-		water.material.SetTextureOffset("_DetailAlbedoMap", new Vector2(0, Time.time / 80));
+		//water.material.mainTextureOffset = new Vector2(Time.time / 100, 0);
+		//water.material.SetTextureOffset("_DetailAlbedoMap", new Vector2(0, Time.time / 80));
 
 	}
 }
