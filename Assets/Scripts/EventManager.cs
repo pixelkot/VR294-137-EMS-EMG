11e57c082f82f08e3ceb6eb13b5852208c4f6e4d
Kostoeva <irmareka@gmail.com>
2019-12-01 16:46:59 -0800

    adding event manager system + UI set up

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public GameObject UI1;
+    public GameObject UI2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
