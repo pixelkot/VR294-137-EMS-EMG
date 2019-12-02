74ed1dc8aceec4cb3caf1a9cf85381a968ca8962
Kostoeva <irmareka@gmail.com>
2019-12-01 21:54:31 -0800

    adding shooting script and animation and sound to shotgun

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -7,6 +7,9 @@ public class EventManager : MonoBehaviour
     public GameObject UI1;
     public GameObject UI2;
 
+    public GameObject handgun;
+    public GameObject shotgun;
+
     public bool holdingGun = false;
 
     // Start is called before the first frame update
@@ -17,11 +20,21 @@ public class EventManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(holdingGun)
+        if (handgun.GetComponent<OVRGrabbable>().isGrabbed || shotgun.GetComponent<OVRGrabbable>().isGrabbed)
+        {
+            holdingGun = true;
+            Debug.Log("grabbed gun");
+        } else
+        {
+            Debug.Log("gun dropped");
+        }
+
+        if (holdingGun)
         {
             UI1.SetActive(false);
             UI2.SetActive(true);
-        } else
+        }
+        else
         {
             UI1.SetActive(true);
             UI2.SetActive(false);
---
+++ Assets/Scripts/ShotgunShoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform barrelLocation;
+
+    public float shotPower = 100f;
+
+    public AudioSource audioSource;
+    public AudioClip shotgunShot;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            GetComponent<Animation>()["Reload"].wrapMode = WrapMode.Once;
+            GetComponent<Animation>().Play("Reload");
+            Shoot();
+        }
+    }
+
+
+    void Shoot()
+    {
+        audioSource.PlayOneShot(shotgunShot, 0.7F);
+        if (audioSource.isPlaying)
+        {
+            Debug.Log("playing audio");
+        }
+        GameObject bullet = Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation);
+        bullet.transform.localScale = new Vector3(0.04f, 0.04f, 0.04f);
+        bullet.GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
+    }
+}
